34fcc9dd84dc18c185682816ea87b4d06b839d92
mwongess <amosmwongelah@gmail.com>
2023-08-30 15:49:06 +0300

    highlight data

---
+++ src/data/mwongess.ts
@@ -0,0 +1,90 @@
+export const mwongess = {
+  personal: {
+    name: "Amos Mwongela",
+    proffession: "Software Engineer",
+    github: "mwongess",
+    about: ``,
+    socials: {
+      in: "mwongess",
+      X: "mwongess",
+    },
+    contacts: {
+      phone: "+2547 429 719 32",
+      email: "amosmwongelah@gmail.com",
+    },
+  },
+
+  skills: {
+    devops: ["Git", "Github", "Docker", "Azure", "AWS", "Appwrite", "Firebase"],
+    web: [
+      "HTML",
+      "CSS",
+      "Tailwind",
+      "Javascript",
+      "Typescript",
+      "React",
+      "NextJs",
+      "Angular",
+      "Sveltekit",
+      "Node",
+      "Express",
+      "Fastify",
+      "NestJS",
+    ],
+    programming: "C#",
+  },
+  projects: [
+    {
+      name: "multiframe-shop",
+      description: "Same app, different frameworks🎉❤️",
+      technologies: ["NextJs", "Sveltekit", "Tailwind", "Appwrite"],
+    },
+    {
+      name: "plan-dash",
+      description:
+        "A platform designed to efficiently manage projects of all sizes and complexities. 📌📅",
+      technologies: [
+        "React",
+        "Typescript",
+        "Tailwind",
+        "Node",
+        "ReactQuery",
+        "Node",
+        "MSSQL",
+        "Azure",
+      ],
+    },
+    {
+      name: "connectify",
+      description:
+        "A comprehensive application designed to provide users with a social networking experience🌐👋",
+      technologies: [
+        "React",
+        "Typescript",
+        "Tailwind",
+        "Node",
+        "Socket.io",
+        "MSSQL",
+      ],
+    },
+    {
+      name: "mssql-mate",
+      description:
+        "An exciting npm package ,empowering effortless connectivity, seamless querying, and the smooth execution of procedures within an SQL Server database. Enjoy the power and ease! 🔥🎉",
+      technologies: ["Node", "Typescript", "MSSQL"],
+    },
+    { name: "ngozi-natural", description: "" },
+    {
+      name: "send-it",
+      description:
+        "Platform designed to simplify the process of shipping items by providing you with accurate quotes tailored to the weight of your products.Also intergrated with map for easy tracking.",
+      technologies: ["Angular", "CSS", "Node", "Express", "MSSQL"],
+    },
+    { name: "water-tracker", description: "" },
+    {
+      name: "store-sync",
+      description: "Admin app to manage your online store !",
+      technologies: ["Angular", "Angular-Material", "Tailwind", "Appwrite"],
+    },
+  ],
+};
