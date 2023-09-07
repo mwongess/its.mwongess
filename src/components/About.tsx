d0d8b5be2adeb9f972a4119a925734c467cc95aa
mwongess <amosmwongelah@gmail.com>
2023-09-07 23:34:18 +0300

    add typing text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,48 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const About = () => {
+  const [text, setText] = useState("")
+  const professions = ["Fullstack Developer |", "Bsc Software Engineering Graduate |"]
+  const delay = 100
+
+  useEffect(() => {
+    let currentIndex = 0;
+    let currentText = '';
+    let isDeleting = false;
+
+    const type = () => {
+      const currentWord = professions[currentIndex];
+      if (isDeleting) {
+        currentText = currentWord.substring(0, currentText.length - 1);
+      } else {
+        currentText = currentWord.substring(0, currentText.length + 1);
+      }
+
+      setText(currentText);
+
+      let typingSpeed = delay;
+
+      if (isDeleting) {
+        typingSpeed /= 2; // Adjust backspace speed
+      }
+
+      if (!isDeleting && currentText === currentWord) {
+        typingSpeed = 1000; // Delay after typing complete
+        isDeleting = true;
+      } else if (isDeleting && currentText === '') {
+        isDeleting = false;
+        currentIndex = (currentIndex + 1) % professions.length;
+      }
+
+      setTimeout(type, typingSpeed);
+    };
+
+    setTimeout(type, delay);
+  }, []);
+
   return (
     <div id='about' className='sm:flex justify-between items-center pt-20 min-h-screen'>
       {/* <div className='relative h-72 w-72 rounded-full'>
@@ -14,7 +55,7 @@ const About = () => {
       <div className='sm:w-1/2'>
         <h1 className='font-bold text-3xl mb-12'>About Me</h1>
         <div>
-          <h1 className='mb-2 text-xl'>I&apos;m Amos Mwongela and I&apos;m a  <span className='font-bold'>Full Stack Developer</span></h1>
+          <h1 className='mb-2 text-xl'>I&apos;m Amos Mwongela and I&apos;m a  <span className='font-bold text-[#23ce6b] text-2xl'>{text}</span></h1>
           <p className='text-lg'>My expertise spans across the entire software development stack, allowing me to tackle challenges with a holistic approach. From front-end design to back-end infrastructure, I take pride in delivering comprehensive solutions that not only meet but exceed the needs of users and stakeholders alike.</p>
         </div>
       </div>
