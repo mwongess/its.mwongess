a8f9d4c37bdbf60e8c2a67d60c6c63007db21f47
mwongess <amosmwongelah@gmail.com>
2023-09-07 23:51:22 +0300

    got to contact me

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,15 @@
 "use client"
 
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const Hero = () => {
+  const router = useRouter()
+  const goToContact = () => {
+    router.push("#contact")
+  }
   return (
     <div className='flex items-center justify-between'>
       <div className='flex flex-col gap-5'>
@@ -14,7 +20,7 @@ const Hero = () => {
         <div>
           <p className='proffession font-bold text-xl'>Fullstack Software Developer</p>
           <p className='text-lg'>Talented full-stack software engineer with a passion for creating elegant, robust, and scalable applications.</p>
-          <button className='contact-me rounded-xl py-4 px-8 w-1/2 mt-8'>Contact</button>
+          <button onClick={goToContact} className='contact-me rounded-xl py-4 px-8 w-1/2 mt-8'>Contact</button>
         </div>
       </div>
       <Image className="hidden sm:block animated fadeInRight" src="/illustration.svg" priority height={200} width={500} quality={100} alt='illustration' />
