80447c259f37855d2d829e16276a3f2372f6a8dd
mwongess <amosmwongelah@gmail.com>
2023-09-10 15:39:54 +0300

    fix broken route link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,12 +28,12 @@ const Header = () => {
     <div className={`sticky z-10 top-0 flex justify-between  items-center py-5 px-4 sm:px-[7rem]  ${scrolled && "scrolled" } `}>
       <div className="logo font-bold text-3xl sm:text-4xl">mwonge<span className=''>ss</span></div>
       <div className='hidden sm:flex items-center gap-8 font-bold text-sm'>
-        <Link href="#home">HOME</Link>
+        <Link href="/#home">HOME</Link>
         <Link href="/json">JSON</Link>
-        <Link href="#about">ABOUT</Link>
-        <Link href="#skills">SKILLS</Link>
-        <Link href="#projects">PROJECTS</Link>
-        <Link href="#contact">CONTACT</Link>
+        <Link href="/#about">ABOUT</Link>
+        <Link href="/#skills">SKILLS</Link>
+        <Link href="/#projects">PROJECTS</Link>
+        <Link href="/#contact">CONTACT</Link>
         <Link href="">CV</Link>
       </div>
       <p className='block sm:hidden text-3xl'>
