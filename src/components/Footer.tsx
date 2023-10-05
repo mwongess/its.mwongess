9c659539fbd2b5d124534c7acf5b7a178a70df28
mwongess <amosmwongelah@gmail.com>
2023-10-05 21:57:35 +0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-center bg-black text-white p-4'>
-      <p>2023.Made with ❤️ by <Link href="">Amos Mwongela.</Link></p>
+      <p>2023.Made with ❤️ by <Link href="https://www.linkedin.com/in/mwongess/">Amos Mwongela.</Link></p>
     </div>
   )
 }
