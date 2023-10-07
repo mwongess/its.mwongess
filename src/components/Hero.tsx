c82a8af700b77329160a3db8e91cd594f420efca
mwongess <amosmwongelah@gmail.com>
2023-10-07 20:14:03 +0300

    bring your ideas to life

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,16 +24,19 @@ const Hero = () => {
         </div>
       </div>
       <div className='relative  w-1/2'>
-        
+
         <Image className="hidden sm:block animated fadeInRight " src="/illustration.svg" priority height={200} width={500} quality={100} alt='illustration' />
         <div className='flex  justify-center flex-col absolute rounded-l-xl gap-1 top-1/2 left-[44%] bg-white p-4 hover:scale-105  cursor-pointer'>
           <p className='relative h-[3rem] w-[3rem] rounded-full bg-red-300'>
-            <Image className='rounded-full' src="https://avatars.githubusercontent.com/u/86522089?v=4" fill alt='profile'/>
+            <Image className='rounded-full' src="https://avatars.githubusercontent.com/u/86522089?v=4" fill alt='profile' />
           </p>
           <p className='font-bold'>Amos Mwongela G</p>
           <p className='text-[#23ce6b]'>@mwongess</p>
           <p>The Versatile Developer.</p>
         </div>
+        {/* <div className='flex  justify-center flex-col absolute rounded-xl gap-1 top-1/4 left-1/2  shadow-md bg-white p-4 hover:scale-105  cursor-pointer'>
+          <p>Bringing your ideas to life through code.</p>
+        </div> */}
       </div>
     </div>
   )
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div id='skills' className='flex flex-col items-center justify-center sm:min-h-screen gap-4 gap-8 sm:gap-20'>
-      <h1 className='font-bold text-4xl '>My Skillset</h1>
+    <div id='skills' className='flex flex-col items-center justify-center sm:min-h-screen gap-8 sm:gap-20'>
+      <div className='flex gap-3 items-center'>
+        <h1 className=' font-bold text-4xl '>My Skill set</h1>
+        <p className='border border-blue-800 p-2 rounded-full font-normal text-sm text-blue-800 cursor-pointer hover:scale-105 h-fit'>Bring your ideas to life!</p>
+      </div>
       <img src="https://skillicons.dev/icons?i=git,github,githubactions,visualstudio,,html,css,tailwind,js,ts,react,next,,aws,azure,appwrite,firebase,stackoverflow,webpack,figma,codepen,,vite,angular,svelte,nodejs,express,nestjs,mongodb,,mysql,postman,docker,kubernetes,vim,linux,netlify,powershell,,py,php,redux,cs,dotnet,wordpress,graphql,,postgres,ai,md,discord&amp;perline=17" />
     </div>
   )
