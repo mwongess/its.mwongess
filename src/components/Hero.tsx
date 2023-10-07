ca368013a966eb77f3fc292abfa34d525c5aaa2b
mwongess <amosmwongelah@gmail.com>
2023-10-07 20:32:02 +0300

    update mobile responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
   }
   return (
     <div className='flex items-center justify-between gap-4'>
-      <div className='flex flex-col gap-5 w-1/2'>
+      <div className='flex flex-col gap-5 w-full sm:w-1/2'>
         <div>
           <h1 className='text-lg'>Hello 👋, I am</h1>
           <h1 className='font-bold text-3xl sm:text-5xl sm:mt-3'>Amos Mwongela</h1>
@@ -23,9 +23,8 @@ const Hero = () => {
           <button onClick={goToContact} className='hover:scale-105  font-bold contact-me rounded-xl py-4 px-8 w-1/2 mt-8'>See My Projects</button>
         </div>
       </div>
-      <div className='relative  w-1/2'>
-
-        <Image className="hidden sm:block animated fadeInRight " src="/illustration.svg" priority height={200} width={500} quality={100} alt='illustration' />
+      <div className='relative  w-1/2 hidden sm:block'>
+        <Image className=" animated fadeInRight " src="/illustration.svg" priority height={200} width={500} quality={100} alt='illustration' />
         <div className='flex  justify-center flex-col absolute rounded-l-xl gap-1 top-1/2 left-[44%] bg-white p-4 hover:scale-105  cursor-pointer'>
           <p className='relative h-[3rem] w-[3rem] rounded-full bg-red-300'>
             <Image className='rounded-full' src="https://avatars.githubusercontent.com/u/86522089?v=4" fill alt='profile' />
