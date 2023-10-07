ac60657560bfdc77deb15b7e76de7c6445f412da
mwongess <amosmwongelah@gmail.com>
2023-10-07 12:14:31 +0300

    add skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,7 +4,7 @@ const Skills = () => {
   return (
     <div id='skills' className='flex flex-col items-center justify-center sm:min-h-screen gap-4 gap-8 sm:gap-20'>
       <h1 className='font-bold text-4xl '>My Skillset</h1>
-      <img src="https://skillicons.dev/icons?i=git,github,githubactions,visualstudio,,html,css,tailwind,js,ts,react,next,,aws,azure,appwrite,firebase,stackoverflow,webpack,figma,codepen,,vite,angular,svelte,nodejs,express,nestjs,mongodb,,mysql,postman,docker,kubernetes,vim,linux,netlify,powershell,,redux,cs,dotnet,discord&amp;perline=17" />
+      <img src="https://skillicons.dev/icons?i=git,github,githubactions,visualstudio,,html,css,tailwind,js,ts,react,next,,aws,azure,appwrite,firebase,stackoverflow,webpack,figma,codepen,,vite,angular,svelte,nodejs,express,nestjs,mongodb,,mysql,postman,docker,kubernetes,vim,linux,netlify,powershell,,py,php,redux,cs,dotnet,wordpress,graphql,,postgres,ai,md,discord&amp;perline=17" />
     </div>
   )
 }
