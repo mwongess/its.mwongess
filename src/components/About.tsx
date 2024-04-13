86bcd9f8957b1d076c281b0def202bf75dd47a95
mwongess <amosmwongelah@gmail.com>
2024-04-13 21:18:46 +0300

    br

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -51,7 +51,7 @@ const About = () => {
       <div className='sm:w-1/2'>
         <h1 className='font-bold text-3xl mb-8 sm:mb-12'>About Me</h1>
         <div>
-          <h1 className='mb-2 text-xl'>I&apos;m Amos Mwongela and I&apos;m a  <span className='font-bold text-[#23ce6b] text-2xl'>{text}</span></h1>
+          <h1 className='mb-2 text-xl'>I&apos;m Amos Mwongela <br/> and I&apos;m a <span className='font-bold text-[#23ce6b] text-2xl'>{text}</span></h1>
           <p className='text-lg'>My expertise spans across the entire software development stack, allowing me to tackle challenges with a holistic approach. From front-end design to back-end infrastructure, I take pride in delivering comprehensive solutions that not only meet but exceed the needs of users and stakeholders alike.</p>
         </div>
       </div>
