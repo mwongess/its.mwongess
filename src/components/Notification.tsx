da364a63b1be96e0df6975d29b0e428c942487c2
Amos Mwongela <amosmwongelah@gmail.com>
2024-06-01 22:06:09 +0300

    Update Notification.tsx

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -19,13 +19,12 @@ const Notification = () => {
             className="flex items-center text-center justify-between p-1 bg-gradient-to-r via-[#2a6049] from-[#1e1e28] to-[#1e1e28] relative">
             <p></p>
             <p>
-              Currently learning building{' '}
+              Currently learning building with{' '}
               <span className="underline">
                 <a href="https://en.wikipedia.org/wiki/Java_(programming_language)" target="_blank" rel="noopener noreferrer">
                   Java
                 </a>
               </span>{' '}
-              daps!
             </p>
             <button onClick={handleClose} className="self-end text-xl mr-4">
               &times;
